beaccab60a5681d9501910714178927e9c0f29dc
harshjohiya <harsh24@iiserb.ac.in>
2026-05-19 18:03:29 +0530

    render ready backend

--- backend/db/database.py
+++ backend/db/database.py
@@ -3,8 +3,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from core.config import config
 
+from pathlib import Path
+
 # We use SQLite by default to make it easy to run
 SQLALCHEMY_DATABASE_URL = "sqlite:///./data/trace.db"
+
+# Ensure data directory exists
+Path("./data").mkdir(parents=True, exist_ok=True)
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
